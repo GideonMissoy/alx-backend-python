f8233525e4692454f7cbb6b715c38bd2450078dd
MGK-Kagai <swifttask001@gmail.com>
2024-06-08 00:36:07 +0300

    3. Basic annotations - to string.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''type-annotated function to_str that takes a float n'''
+
+
+def to_str(n: float) -> str:
+    '''returns the string representation of the float.'''
+    return str(n)
