6d864591f246d5f49b52e3f0a88a16bbf34a6ee0
MGK-Kagai <swifttask001@gmail.com>
2024-06-11 00:46:14 +0300

    1. Let's execute multiple coroutines at the same time with async.

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+Writes an async routine that takes in 2 int arguments (in this order): 
+n and max_delay. You will spawn wait_random n times with the specified max_delay.
+'''
+
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ Wait_n should returns list of actual delays """
+    spawn_ls = []
+    delay_ls = []
+    for i in range(n):
+        delayed_task = asyncio.create_task(wait_random(max_delay))
+        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
+        spawn_ls.append(delayed_task)
+
+    for spawn in spawn_ls:
+        await spawn
+
+    return delay_ls
